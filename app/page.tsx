d8c4a8d590874d425db4a0fa53a72164d3363dd3
calvin <calvinunsafe@gmail.com>
2025-11-24 00:46:20 +0100

    adding assetlink.json

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import CTA from '@/components/CTA'
 import Footer from '@/components/Footer'
 
 export default function Home() {
-  return (
+  return ( 
     <main className="min-h-screen">
       <Header />
       <Hero />
