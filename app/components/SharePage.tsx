c8789d03c45265fac22ec5645d6f56254b69ac5a
calvin <calvinunsafe@gmail.com>
2026-04-28 21:35:56 +0000

    run

--- app/components/SharePage.tsx
+++ app/components/SharePage.tsx
@@ -864,7 +864,7 @@ const handlePlatformShare = async (platformId: string) => {
       )}
 {/* The Help Modal */}
 {showHelpModal && (
-  <div className="fixed inset-0 z-50 flex items-start justify-start bg-black/40 px-6 pt-32">
+  <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 px-6">
     <ShareModal
       isOpen={showHelpModal} 
       onClose={() => setShowHelpModal(false)} 
